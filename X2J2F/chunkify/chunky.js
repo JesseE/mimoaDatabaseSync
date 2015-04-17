25e1520b0daf7daac4adba24c30c4d1ca2ec46b8
Jesse Eikema <jesseeikema@Jesses-MacBook-Pro.local>
2015-04-17 15:17:40 +0200

    optimized for 256

--- X2J2F/chunkify/chunky.js
+++ X2J2F/chunkify/chunky.js
@@ -5,23 +5,20 @@
  * Created by jesse on 27/02/15.
  */
 var async = require('async');
-var fs = require('fs');
+var fs = require('graceful-fs');
 var xml2js = require('xml2js');
 var parser = new xml2js.Parser();
 var http = require('http');
 var url = 'http://www.mimoa.eu/widgets/mapps/';
 var MongoClient = require('mongodb').MongoClient;
-var exec = require('child_process').exec,
-    child;
-var proc = require('child_process').spawn('mongod');
 var schedule = require('node-schedule');
 //setup timed function each night
-var runFunction = schedule.scheduleJob({hour: 1, minute: 00}, function(){
-    removeFilesInDirectory();
-});
+//var runFunction = schedule.scheduleJob({hour: 1, minute: 00}, function(){
+//    removeFilesInDirectory();
+//});
+removeFilesInDirectory();
 // there are files in this directory remove them
 function removeFilesInDirectory() {
-    proc.kill('SIGINT');
     fs.readdir('X2J2F/jsonOutput', function(err, res) {
         if(err) { throw err;} else {
             var files = res;
@@ -94,13 +91,7 @@ function fetchXMLFile() {
 //return json file for each document item from the xml feed
 function writeXMLChunks(xmlFileData){
     for (var i = 0; i < xmlFileData.length; i++) {
-        fs.appendFile("X2J2F/jsonOutput/mimoaProjects" + i + ".json", JSON.stringify(xmlFileData[i]), function (err, res) {
-            if (err) {
-                console.log(err);
-            } else {
-
-            }
-        });
+        fs.appendFileSync("X2J2F/jsonOutput/mimoaProjects" + i + ".json", JSON.stringify(xmlFileData[i]));
     }
     connectToDB();
 }
@@ -108,39 +99,44 @@ function writeXMLChunks(xmlFileData){
 function connectToDB() {
     MongoClient.connect("mongodb://JesseEikema:Eikema23@ds061651.mongolab.com:61651/mimoa", function(err, db) {
        if(err){
+           console.log(err);
             throw err;
        } else {
            console.log('find collection and reset');
-           //uploadToMongolab();
+
                if(err){throw err;}else{
                    console.log('there are no documents start uploading to mongolab');
                    fs.readdir('X2J2F/jsonOutput', function (err, files) {
                        if (err) {
+                           console.log(err);
                            throw err;
                        } else {
                            async.forEach(files, function (file){
                                fs.readFile('X2J2F/jsonOutput/' + file, 'utf-8', function (err, data) {
                                    if (err) {
+                                       console.log(err);
                                        throw err;
                                    } else {
                                        console.log('do JSON magic and Mongo Magic');
                                        var dirtyString = data;
                                        var cleanString = dirtyString.replace(/[$]/g, "value");
                                        var JSONobject = JSON.parse(cleanString);
-                                       db.collection('mimoaDatabase').update({id:JSONobject.id},JSONobject,{upsert:true},{multi:true}, function (err, res) {
-                                           if (err) {
-                                              console.log('this is where it fails' +err);
-                                           } else {
-                                               console.log('inserting into db');
+                                       //if(mimoaDatabase.count > 8000) {
+                                       db.collection('mimoaDatabase').update({id: JSONobject.id}, JSONobject, {upsert: true}, {multi: true}, function (err, res) {
+                                              if(err) {console.log(err);} else {
 
-                                           }
+                                              }
                                        });
+                                       //}else{
+                                       //  db.collection('mimoaDatabase').remove();
+                                       //}
+
                                    }
                                });
                            }, function(err,res){
+                               db.close();
                                if(err) {throw err;} else{
                                    console.log('no magic was need');
-
                                }
                            });
 
@@ -151,13 +147,3 @@ function connectToDB() {
        }
     });
 }
-//function uploadToMongolab() {
-//    child = exec('for filename in X2J2F/jsonOutput/*.json; do mongoimport -h ds061651.mongolab.com:61651 -d mimoa -c mimoaDatabase -u JesseEikema -p Eikema23 --file $filename; done',{maxBuffer: 1024 * 500},
-//    function (error, res) {
-//        if (error !== null) {
-//            console.log('exec error: ' + error);
-//        } else {
-//            console.log('currently uploading new docs');
-//        }
-//    });
-//}
